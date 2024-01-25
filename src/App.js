6d694b27a63cf500279cf136d69ab8621b65e27f
ombahiwal <ombahiwal@gmail.com>
2024-01-25 14:36:20 +0100

    Color and radio select

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Heatmap from './Heatmap';
 
-function App() {
+const App = () => {
+  const [heatmapPoints, setHeatmapPoints] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [circles, setCircles] = useState([]);
+  const [indexValue, setIndexValue] = useState('greeness');
+
+  const handleRadioChange = (event) => {
+    setIndexValue(event.target.value);
+  };
+
+  useEffect(() => {
+    const fetchHeatmapData = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:5000/locations');
+        const data = response.data;
+        setHeatmapPoints(data.locations.map(location => [location.geoCoordinate.latitude, location.geoCoordinate.longitude]));
+        console.log(data);
+        const markersArray = data.locations.map(location => ({
+          indexValue: location[indexValue],
+          popupContent: location.placename,
+          center: [location.geoCoordinate.latitude, location.geoCoordinate.longitude],
+          radius: 300,
+          position: { lat: location.geoCoordinate.latitude, lng: location.geoCoordinate.longitude }
+        }));
+
+        console.log(markersArray);
+        setCircles(markersArray);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching heatmap data:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchHeatmapData();
+  }, [indexValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>InfInt</h1>
+      <div>
+        <label>
+          <input
+            type="radio"
+            value="greeness"
+            checked={indexValue === 'greeness'}
+            onChange={handleRadioChange}
+          />
+          Greenness
+        </label>
+
+        <label>
+          <input
+            type="radio"
+            value="walkability"
+            checked={indexValue === 'walkability'}
+            onChange={handleRadioChange}
+          />
+          Walkability
+        </label>
+
+        <label>
+          <input
+            type="radio"
+            value="aqi"
+            checked={indexValue === 'aqi'}
+            onChange={handleRadioChange}
+          />
+          AQI
+        </label>
+
+        <label>
+          <input
+            type="radio"
+            value="transitscore"
+            checked={indexValue === 'transitscore'}
+            onChange={handleRadioChange}
+          />
+          Transit Score
+        </label>
+      </div>
+
+      {loading ? (
+        <p>Loading heatmap data...</p>
+      ) : (
+        <Heatmap center={heatmapPoints[0]} zoom={10} points={heatmapPoints} circles={circles} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Heatmap.js
@@ -0,0 +1,83 @@
+import React, { useEffect } from 'react';
+import { MapContainer, TileLayer, useMap, Circle, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet.heat/dist/leaflet-heat.js';
+import L from 'leaflet';
+
+const GRADIENT = {          // Gradient colors for the heatmap
+  0.4: 'blue',
+  0.6: 'cyan',
+  0.7: 'lime',
+  0.8: 'yellow',
+  1.0: 'red'
+};
+
+const reversedGradient = {
+  0.4: 'red',
+  0.6: 'yellow',
+  0.7: 'lime',
+  0.8: 'cyan',
+  1.0: 'blue'
+}
+function getColor(value) {
+  
+  const colorMap = reversedGradient;
+  const keys = Object.keys(colorMap);
+  let closestKey = keys.reduce((a, b) => Math.abs(b - value) < Math.abs(a - value) ? b : a);
+
+  return colorMap[closestKey];
+}
+
+const HeatmapLayer = ({ points }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (points && points.length > 0) {
+      const heatLayer = L.heatLayer(points).addTo(map);
+      return () => map.removeLayer(heatLayer);
+    }
+  }, [map, points]);
+
+  return null;
+};
+
+const CirclesLayer = ({ circles }) => {
+  return (
+    <>
+      {circles.map((circle, index) => (
+        <Circle
+          key={index}
+          center={circle.center}
+          radius={circle.radius}
+          pathOptions={{ color: getColor(circle.indexValue), fillColor: getColor(circle.in) }}  // Customize the circle color
+        >
+          <Popup>{circle.popupContent}</Popup>
+        </Circle>
+      ))}
+    </>
+  );
+};
+
+
+const Heatmap = ({ center, zoom, points, circles }) => {
+  const heatmapOptions = {
+    radius: 14,          // Radius of each data point
+    blur: 30,            // Blur radius (higher values create smoother heatmap)
+    maxZoom: 17,         // Maximum zoom level for the heatmap
+    gradient: GRADIENT,
+    maxOpacity: 2,     // Maximum opacity of the heatmap
+    minOpacity: 0.5      // Minimum opacity of the heatmap
+  };
+  return (
+    <MapContainer center={center} zoom={zoom} style={{ height: '400px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <HeatmapLayer points={points} options={heatmapOptions}/>
+      {circles && <CirclesLayer circles={circles} />}
+    </MapContainer>
+  );
+};
+
+export default Heatmap;
